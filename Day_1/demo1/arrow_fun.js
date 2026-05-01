d29b9b43a84505e90cbd47492e5e8bf9ffc38341
rohitt <164724029+Rohit-1o@users.noreply.github.com>
2026-05-01 14:54:33 +0530

    Update arrow_fun.js

--- Day_1/demo1/arrow_fun.js
+++ Day_1/demo1/arrow_fun.js
@@ -2,4 +2,5 @@ let result =(num1,num2,num3)=>{ //arrow function
     //return (num1+num2+num3); Method 1
     return `${num1} + ${num2} + ${num3} = ${num1+num2+num3}` //Method 2
 };
+
 console.log(result(2,3,5));
